d3ee03746d7d29bb95cf61ed5485c62dbd9db33c
Daniel Miladinov <daniel.miladinov@gmail.com>
2013-05-26 14:16:11 -0400

    Fun with procedures and methods that return Unit.
    Methods f and g, even though they look like they should return a
    string, do not, because of how they were declared (to return Unit).
    
    Only h() will return a string (though you should specify the return
    type, scala can infer that h() does in fact return a string, as it's
    the last expression produced by the method and there's an equals sign
    indicating a return).

--- src/ChecksumAccumulator.scala
+++ src/ChecksumAccumulator.scala
@@ -3,4 +3,7 @@ class ChecksumAccumulator {
   private var sum = 0
   def add (b: Byte) { sum += b }
   def checksum(): Int = ~(sum & 0xFF) + 1
+  def f(): Unit = "this String gets lost"
+  def g() { "this String gets lost too" }
+  def h() = { "this String gets returned!" }
 }
