2ef3c9f5faaf567af5a66916df520afb4c60a342
Daniel Miladinov <daniel.miladinov@gmail.com>
2013-05-29 01:18:42 -0400

    Subtract Rationals or Ints from Rationals.

--- src/Rational.scala
+++ src/Rational.scala
@@ -19,6 +19,15 @@ class Rational(n: Int, d: Int) {
   def + (i: Int): Rational =
     new Rational(numerator + i * denominator, denominator)
 
+  def - (that: Rational): Rational =
+    new Rational(
+      numerator * that.denominator - that.numerator * denominator,
+      denominator * that.denominator
+    )
+
+  def - (i: Int): Rational =
+    new Rational(numerator - i * denominator, denominator)
+
   def * (that: Rational): Rational =
     new Rational(numerator * that.numerator, denominator * that.denominator)
 
