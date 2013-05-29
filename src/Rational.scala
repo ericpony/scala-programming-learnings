c9393b8154f69e91230e05119495a0b46ff1f211
Daniel Miladinov <daniel.miladinov@gmail.com>
2013-05-29 00:09:08 -0400

    Add method add(that: Rational) to Rational.

--- src/Rational.scala
+++ src/Rational.scala
@@ -4,4 +4,9 @@ class Rational(n: Int, d: Int) {
   val numerator: Int = n
   val denominator: Int = d
   override def toString = numerator + "/" + denominator
+  def add(that: Rational): Rational =
+    new Rational(
+      numerator * that.denominator + that.numerator * denominator,
+      denominator * that.denominator
+    )
 }
