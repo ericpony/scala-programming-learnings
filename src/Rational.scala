9b200f5fa03b1b910a0741617a311f76fd456e59
Daniel Miladinov <daniel.miladinov@gmail.com>
2013-05-29 01:05:29 -0400

    Add some vertical space to Raional's source.
    It was starting to get just a little too crowded for my taste.

--- src/Rational.scala
+++ src/Rational.scala
@@ -1,20 +1,27 @@
 
 class Rational(n: Int, d: Int) {
   require(d != 0)
+
   private val g = gcd(n.abs, d.abs)
   val numerator = n / g
   val denominator = d / g
+
   def this(n: Int) = this(n, 1)
+
   override def toString = numerator + "/" + denominator
+
   def add(that: Rational): Rational =
     new Rational(
       numerator * that.denominator + that.numerator * denominator,
       denominator * that.denominator
     )
+
   def lessThan(that: Rational) =
     numerator * that.denominator < that.numerator * denominator
+
   def max(that: Rational) =
     if (lessThan(that)) that else this
+
   private def gcd(a: Int, b: Int): Int =
     if (b == 0) a else gcd(b, a % b)
 }
