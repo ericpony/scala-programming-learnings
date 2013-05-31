11b3c5727d1c6fd36f17b8265c9f5a30403ac396
Daniel Miladinov <daniel.miladinov@gmail.com>
2013-05-30 23:44:27 -0400

    You can do nested iteration within a single for expression.
    If you add multiple <- clauses, you will get nested "loops". For
    example, the for expression here has two nested loops. The outer
    loop iterates through filesHere, and the inner loop iterates
    through fileLines(file) for any file that ends with .scala.
    
    If you use curly braces instead of parentheses to surround the
    generators and filters, you can leave off the semicolons that
    are otherwise needed inside of parentheses (where they are not
    inferred).

--- examples/for_expressions.scala
+++ examples/for_expressions.scala
@@ -11,3 +11,19 @@ for (i <- 1 to 4)
 
 for (i <- 1 until 4)
   println("Iteration " + i)
+
+
+def fileLines(file: java.io.File) =
+  scala.io.Source.fromFile(file).getLines().toList
+
+def grep(pattern: String) {
+  for {
+    file <- filesHere
+    if file.getName.endsWith(".scala")
+
+    line <- fileLines(file)
+    if line.trim.matches(pattern)
+  } println(file + ": " + line.trim)
+}
+
+grep(".*gcd.*")
