59561873bbe6b19742b2f9c7768462c27ad29c08
Daniel Miladinov <daniel.miladinov@gmail.com>
2013-05-30 23:33:42 -0400

    Filter the for expression in a functional style.
    What makes this form most different from the imperative form is
    that this expression can result in an interesting value,
    a collection whose type is determined by the for expression's <-
    clauses.

--- examples/for_expressions.scala
+++ examples/for_expressions.scala
@@ -1,8 +1,7 @@
 val filesHere = (new java.io.File(".")).listFiles
 
-for (file <- filesHere)
-  if (file.getName.endsWith(".scala"))
-    println(file)
+for (file <- filesHere if file.getName.endsWith(".scala"))
+  println(file)
 
 for (i <- 1 to 4)
   println("Iteration " + i)
