53176ba559282f892a04b960ea02c4eec65fb34e
Daniel Miladinov <daniel.miladinov@gmail.com>
2013-05-31 21:40:36 -0400

    Catching exceptions in Scala has a slightly different syntax.
    The syntax for catch clauses was chosen for its consistency with
    an important part of Scala - pattern matching.
    
    The behavior of this try-catch expression is the same as in other
    languages with exceptions. The body is executed, and if it throws
    an exception, each catch clause is tried in turn.
    
    In this examle, if the exception is of type FileNotFoundException,
    the first clause will excecute. If it is of type IOException, the
    second clause will execute. If the exception is of neither type,
    the try-catch will terminate and the exception will propogate
    further.
    
    Unlike java, Scala doesn't require you to catch checked exceptions,
    or declare them in a throws clause. You can declare a throws
    clause if you wish with the @throws annotation, but it is not
    required.

---
+++ examples/catching_exceptions.scala
@@ -0,0 +1,10 @@
+import java.io.FileReader
+import java.io.FileNotFoundException
+import java.io.IOException
+
+try {
+  val f = new FileReader("input.txt")
+} catch {
+  case ex: FileNotFoundException => // Handle missing file
+  case ex: IOException => // Handle other I/O error
+}
