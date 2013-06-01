6906adff96318913313c3df9631e7a3d49d127a2
Daniel Miladinov <daniel.miladinov@gmail.com>
2013-05-31 21:34:34 -0400

    In Scala, the throw expression also has a result type.
    here, if n is even, the return value will be half of n. If n is
    not even, an exception will be thrown before anything can be
    returned at all. Because of this, it is safe to treat a thrown
    exception as any kind of value whatsoever. Any context that tries
    to use the return from a throw will never get to do so, and thus
    no harm will come.
    
    Technically, an exception throw has type Nothing. You can use a
    throw as an expression even though it will never actually evaluate
    to anything. This little bit of technical gymnastics might sound
    weird, but is frequently useful in cases such as this. Once branch
    of an if computes a value, while the other throws an exception and
    computes Nothing. The type of the whole if expresssion is then
    the type of the branch which does compute something.

--- examples/throwing_exceptions.scala
+++ examples/throwing_exceptions.scala
@@ -1,2 +1,10 @@
 // Throwing exceptions should look very familiar to java developers:
 throw new IllegalArgumentException
+
+// In Scala, however, throw is an expression that has a result type.
+// The result type matters here:
+def halve(n: Int): Int =
+  if (n % 2 == 0)
+    n / 2
+  else
+    throw new RuntimeException("n must be even")
