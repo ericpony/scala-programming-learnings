695b97d9e39dba1c32ca4a6782dfcdc2a96d2cd3
Daniel Miladinov <daniel.miladinov@gmail.com>
2013-06-02 02:30:52 -0400

    Remove unnecessary parameters from the local function, processLine.
    Helper functions can access the parameters of their enclosing functions.

--- src/LongLines.scala
+++ src/LongLines.scala
@@ -2,12 +2,12 @@ import scala.io.Source
 
 object LongLines {
   def processFile(filename: String, width: Int) {
-    def processLine(filename: String, width: Int, line: String) {
+    def processLine(line: String) {
       if (line.length > width)
         println(filename + " " + line.trim)
     }
 
     val source = Source.fromFile(filename)
-    for (line <- source.getLines()) processLine(filename, width, line)
+    for (line <- source.getLines()) processLine(line)
   }
 }
