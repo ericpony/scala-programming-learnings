cd23728a70148844f89ac18db40193f603b116c3
Daniel Miladinov <daniel.miladinov@gmail.com>
2013-06-02 02:10:49 -0400

    Refactor the multiplication table code to be more functional.

--- examples/multiplication_tables.scala
+++ examples/multiplication_tables.scala
@@ -1,21 +1,20 @@
-def printMultiplicationTable() {
-  var i = 1
-
-  while (i <= 10) {
-    var j = 1
-
-    while (j <= 10) {
-      val prod = (i * j).toString
-      var k = prod.length
-
-      while (k < 4) {
-        print(" ")
-        k += 1
-      }
-      print(prod)
-      j += 1
-    }
-    println()
-    i += 1
+// Return a row as a sequence
+def makeRowSequence(row: Int) =
+  for (col <- 1 to 10) yield {
+    val product = (row * col).toString
+    val padding = " " * (4 - product.length)
+    padding + product
   }
+
+// Return a row as a string
+def makeRow(row: Int) = makeRowSequence(row).mkString
+
+// Return a multiplication table as a string with one row per line
+def multiplicationTable(): String = {
+  val tableSequence =
+    for (row <- 1 to 10)
+    yield makeRow(row)
+  tableSequence.mkString("\n")
 }
+
+println("\n" concat multiplicationTable())
