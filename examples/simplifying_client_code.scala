198253e5c4d142f87bce7015a5cdfb51cd29e5b3
Daniel Miladinov <daniel.miladinov@gmail.com>
2013-06-09 22:11:27 -0400

    Use List.exists + higher-order functions to simplify client code.
    Whereas the previous version of the code was just a poor man's
    implementation of exists, we can instead lean on the libraries
    and use a higher-order function to tersely express the search
    condition (element (that is, underscore) is less than zero).

--- examples/simplifying_client_code.scala
+++ examples/simplifying_client_code.scala
@@ -1,11 +1,6 @@
-// Consider exists, a method that determines whether a passed value is contained in a collection. You could of course
-// search for an element by having a var initialized to false, looping through the collection checking each item,
-// and setting the var to true if you find what you are looking for. Here’s a method that uses this approach
-// to determine whether a passed List contains a negative number:
-def containsNeg(nums: List[Int]): Boolean = {
-  var exists = false
-  for (num <- nums)
-    if (num < 0)
-      exists = true
-  exists
-}
+// Consider exists, a method that determines whether a passed value is contained in a collection.
+def containsNeg(nums: List[Int]): Boolean = nums.exists(_ < 0)
+
+// The exists method represents a control abstraction. It is a special-purpose looping construct provided
+// by the Scala library rather than being built into the Scala language like while or for. In the previous section,
+// the higher-order function, filesMatching, reduces code duplication in the implementation of the object FileMatcher.
