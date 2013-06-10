8e719a90493dd89d28302ee7e919edb8c49dabe5
Daniel Miladinov <daniel.miladinov@gmail.com>
2013-06-09 22:31:42 -0400

    Add more discussion about how currying works.
    The process of "manually" currying function should help solidify
    the concept of what's going on for you automatically.

--- examples/currying.scala
+++ examples/currying.scala
@@ -10,3 +10,27 @@ def curriedSum(x: Int)(y: Int) = x + y
 // What’s happening here is that when you invoke curriedSum, you actually get two traditional function invocations
 // back to back. The first function invocation takes a single Int parameter named x, and returns a function value
 // for the second function. This second function takes the Int parameter y.
+
+// Here’s a function named first that does in spirit what the first traditional
+// function invocation of curriedSum would do:
+def first(x: Int) = (y: Int) => x + y
+
+// Applying 1 to the first function - in other words, invoking the first function and passing in 1 - yields the second
+// function:
+val second = first(1)
+
+// Applying 2 to the second function yields the result:
+second(2)
+// res6: Int = 3
+
+// These first and second functions are just an illustration of the currying process. They are not directly connected
+// to the curriedSum function. Nevertheless, there is a way to get an actual reference to curriedSum's "second"
+// function. You can use the placeholder notation to use curriedSum in a partially applied function expression,
+// like this:
+val onePlus = curriedSum(1)_
+// onePlus: (Int) => Int = <function1>
+
+// The underscore in curriedSum(1)_ is a placeholder for the second parameter list.2 The result is a reference to a
+// function that, when invoked, adds one to its sole Int argument and returns the result:
+onePlus(2)
+// res7: Int = 3
