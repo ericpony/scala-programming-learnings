fc5c28e1c367e42349d6c1e3c91917854210b009
Daniel Miladinov <daniel.miladinov@gmail.com>
2013-06-10 22:13:47 -0400

    Improve the custom assertion implementation by using a by-name parameter.

--- examples/by_name_parameters.scala
+++ examples/by_name_parameters.scala
@@ -8,3 +8,17 @@ def myAssert(predicate: () => Boolean) {
 
 // The definition is fine, but using it is a little awkward:
 myAssert(() => 5 > 3)
+
+// Instead, we'd like to write it like myAssert(5 > 3), but this won't work because of a type mismatch; you're passing
+// a boolean when it's expecting a function that returns boolean.
+
+// So, let's change myAssert's predicate parameter into a by-name parameter, by changing its type, "() => Boolean", into
+// "=> Boolean":
+def byNameAssert(predicate: => Boolean) {
+  if (assertionsEnabled && !predicate)
+    throw new AssertionError
+}
+
+// Now you can leave out the empty parameter in the property you want to assert. The result is that byNameAssert looks
+// exactly like using a built-in control structure:
+byNameAssert(5 > 3)
