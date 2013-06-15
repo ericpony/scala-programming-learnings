e9f4d12e332f7fbdf91840a27a264ff1f1fd2af4
Daniel Miladinov <daniel.miladinov@gmail.com>
2013-06-14 22:26:02 -0400

    Add a method to specify one Element above another.
    This is an interesting first draft, demonstrating how Array
    concatenation works in Scala.
    
    It's typically bad form for a base class to call out its children
    by name; I wonder how or if that's going to change as the book
    progresses.

--- src/Element.scala
+++ src/Element.scala
@@ -2,4 +2,7 @@ abstract class Element {
   def contents: Array[String]
   def height: Int = contents.length
   def width: Int = if (height == 0) 0 else contents(0).length
+
+  def above(that: Element): Element =
+    new ArrayElement(this.contents ++ that.contents)
 }
