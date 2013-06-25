e47844039db2b1c9b52964bc4203e3ec878cc861
Daniel Miladinov <daniel.miladinov@gmail.com>
2013-06-24 22:50:15 -0400

    Show how we can achieve concise access to classes and packages.

--- src/bobsrockets/navigation/Navigator.scala
+++ src/bobsrockets/navigation/Navigator.scala
@@ -1,11 +1,28 @@
 package bobsrockets.navigation {
   package navigation {
-    // In package bobsrockets.navigation
-    class Navigator
+    //  package bobsrockets.navigation
+    class Navigator {
+      // No need to say bobsrockets.navigation.StarMap
+      val map = new StarMap
+    }
+
+    class StarMap
 
     package tests {
       // In package bobsrockets.navigation.tests
       class NavigatorSuite
+    }
+  }
+
+  class Ship {
+    // No need to say bobsrockets.navigation.Navigator
+    val nav = new navigation.Navigator
+  }
+
+  package fleets {
+    class Fleet {
+      // No need to say bobsrockets.Ship
+      def addShip() { new Ship }
     }
   }
 }
