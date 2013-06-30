8bb1a315256a6ff3eccdf77ddeba204a196543e3
Daniel Miladinov <daniel.miladinov@gmail.com>
2013-06-29 20:30:21 -0400

    Add an example of importing all but one member of a package.
    The statement, import bobsdelights.Fruits.{Pear => _, _} has the effect
    of importing all of bobsdelights.Fruits, except for Pear.

--- src/RenamingImports.scala
+++ src/RenamingImports.scala
@@ -1,10 +1,6 @@
-// You can alias your imports, renaming Apple to McIntosh in this example.
-import bobsdelights.Fruits.{Apple => McIntosh, _}
+import bobsdelights.Fruits.{Pear => _, _}
 import java.{sql => S}
 
 object RenamingImports {
-  // After the above import, you can access the same object either via McIntosh or
-  // bobsdelights.Fruits.Apple
-  val newApple = McIntosh
-  val oldApple =  bobsdelights.Fruits.Apple
+  val apple =  Apple
 }
