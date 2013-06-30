eebfc4743b387c3e93652e34b382456df2f9aef8
Daniel Miladinov <daniel.miladinov@gmail.com>
2013-06-29 20:38:08 -0400

    Add an example of how private access in Scala differs from Java.
    In Java classes have access to private members of their inner classes,
    whereas in Scala they do not.
    
    However, it is alright for InnerMost to access Inner's f(), because
    it's happening from within Inner.
    
    So, code can "reach up" to access private members (as long as it's from
    within the class declaring them) but cannot "reach down" to access them,
    because the access would then be from outside of the class declaring the
    private member, regardless of whether or not the attempt was made from a
    containing outer class.

---
+++ src/Outer.scala
@@ -0,0 +1,13 @@
+class Outer {
+  class Inner {
+    private def f() {
+      println("f")
+    }
+    class InnerMost {
+      f() // OK
+    }
+  }
+
+  // (new Inner).f() // error: f is not accessible
+}
+
