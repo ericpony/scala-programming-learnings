63003191f8d4b6a4bcc2e1da06034a8a3fb304d3
Daniel Miladinov <daniel.miladinov@gmail.com>
2013-07-03 00:26:59 -0400

    Ensure that Element widths are equal using Scala's assert method.
    The assert method is defined in the Predef singleton object, whose
    members are automatically imported into every Scala source file.

--- src/Element.scala
+++ src/Element.scala
@@ -38,6 +38,7 @@ abstract class Element {
   def above(that: Element): Element = {
     val this1 = this widen that.width
     val that1 = that widen this.width
+    assert(this1.width == that1.width)
     elem(this1.contents ++ that1.contents)
   }
 
