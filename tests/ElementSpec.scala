7fcab729d2b53d306995f8fd634059548355dcaf
Daniel Miladinov <daniel.miladinov@gmail.com>
2013-07-05 16:21:33 -0400

    Add an example of a BDD FlatSpec.
    The matchers DSL in ScalaTest lets you write tests that practically read
    like English.  This is pretty cool.

---
+++ tests/ElementSpec.scala
@@ -0,0 +1,21 @@
+import org.scalatest.FlatSpec
+import org.scalatest.matchers.ShouldMatchers
+import Element.elem
+
+class ElementSpec extends FlatSpec with ShouldMatchers {
+  "A UniformElement" should "have a width equal to the passed value" in {
+    val element = elem('x', 2, 3)
+    element.width should be (2)
+  }
+
+  it should "have a height equal to the passed value" in {
+    val element = elem('x', 2, 3)
+    element.height should be (3)
+  }
+
+  it should "throw an IAE if passed a negative width" in {
+    evaluating {
+      elem('x', -2, 3)
+    } should produce [IllegalArgumentException]
+  }
+}
