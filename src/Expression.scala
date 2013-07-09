f438e7390acba802cc1d2add40a843f177cc2706
Daniel Miladinov <daniel.miladinov@gmail.com>
2013-07-08 22:35:45 -0400

    Move method simplifyTop to be a member of class Expression.
    I'm not sure if this is exactly what the authors intended to begin with,
    but I have something in mind for this code, and we'll see if it works
    out or not.

--- src/Expression.scala
+++ src/Expression.scala
@@ -1,12 +1,13 @@
-abstract class Expression
-  case class Variable(name: String) extends Expression
-  case class Number(number: Double) extends Expression
-  case class UnaryOperator(operator: String, arg: Expression) extends Expression
-  case class BinaryOperator(operator: String, left: Expression, right: Expression) extends Expression
-
-def simplifyTop(expr: Expression): Expression = expr match {
-  case UnaryOperator("-", UnaryOperator("-", e))  => e // Double negation
-  case BinaryOperator("+", e, Number(0)) => e          // Adding zero
-  case BinaryOperator("*", e, Number(1)) => e          // Multiplying by one
-  case _ => expr
+abstract class Expression {
+  def simplifyTop(expr: Expression): Expression = expr match {
+    case UnaryOperator("-", UnaryOperator("-", e))  => e // Double negation
+    case BinaryOperator("+", e, Number(0)) => e          // Adding zero
+    case BinaryOperator("*", e, Number(1)) => e          // Multiplying by one
+    case _ => expr
+  }
 }
+
+case class Variable(name: String) extends Expression
+case class Number(number: Double) extends Expression
+case class UnaryOperator(operator: String, arg: Expression) extends Expression
+case class BinaryOperator(operator: String, left: Expression, right: Expression) extends Expression
