9ba81a306c609ea726f3cfdc83cb791e8f727954
Daniel Miladinov <daniel.miladinov@gmail.com>
2013-07-08 22:47:01 -0400

    Add another example of wildcard patterns.
    Wildcards can also be used to ignore parts of an object that you do not
    care about. For example, here we don't care what the elements of the
    binary operation are. It just checks whether it is a binary operation at
    all.
    
    Thus the code can just as well use the wildcard pattern for the elements
    of the BinaryOperator, as shown here.

--- src/net/miladinov/patternmatching/PatternMatchExamples.scala
+++ src/net/miladinov/patternmatching/PatternMatchExamples.scala
@@ -8,4 +8,11 @@ object PatternMatchExamples {
       case _ =>
     }
   }
+
+  def embeddedWildCardPattern(expr: Expression) = {
+    expr match {
+      case BinaryOperator(_, _, _) => println(expr + " is a binary operation")
+      case _ => println("It's something else")
+    }
+  }
 }
