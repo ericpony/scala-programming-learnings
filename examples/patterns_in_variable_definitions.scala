4c0e50d1ba4603b60443564069f064f6a537a41a
Daniel Miladinov <daniel.miladinov@gmail.com>
2013-07-18 23:32:30 -0400

    Add an example combining pattern matching on case classes, in variable definitions.

--- examples/patterns_in_variable_definitions.scala
+++ examples/patterns_in_variable_definitions.scala
@@ -8,3 +8,17 @@ val myTuple = (123, "abc")
 val (number, string) = myTuple
 // number: Int = 123
 // string: java.lang.String = abc
+
+// This construct is quite useful when working with case classes.
+// If you know the precise case class you are working with,
+// then you can deconstruct it with a pattern. Here’s an example:
+import net.miladinov.patternmatching.{BinaryOperator, Number}
+
+val expression = new BinaryOperator("*", Number(5), Number(1))
+// exp: BinaryOperator = BinaryOperator(*,Number(5.0),Number(1.0))
+
+val BinaryOperator(op, left, right) = expression
+// op: String = *
+// left: Expr = Number(5.0)
+// right: Expr = Number(1.0)
+
