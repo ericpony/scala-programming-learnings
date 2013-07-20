49f556f6d4ce19b7f48f8383bb1451b650f32236
Daniel Miladinov <daniel.miladinov@gmail.com>
2013-07-19 21:18:38 -0400

    Add another example of a pattern match in a for expression.
    The interesting bit about this example is that even though there
    are three elements in the results list, only apple and orange
    are printed.
    
    Generated values that do not match the pattern are discarded.
    The second element None in the results list does not match the
    pattern Some(fruit); therefore it does not show up in the output.

--- examples/patterns_in_for_expressions.scala
+++ examples/patterns_in_for_expressions.scala
@@ -4,3 +4,12 @@ val capitals = Map("Italy" -> "Rome", "Germany" -> "Berlin", "Norway" -> "Oslo")
 // You can use a pattern match to destructure each key-value pair as you iterate:
 for ((country, city) <- capitals)
   println("The capital of " + country + " is " + city)
+
+// The pair pattern shown above was special because the match against it
+// can never fail. // Indeed, `capitals` yields a sequence of pairs, so you can
+// be sure that every generated // pair can be matched against a pair pattern.
+// But it is equally possible that a pattern // might not match a generated value.
+// Here is an example where that is the case:
+val results = List(Some("apple"), None, Some("orange"))
+
+for (Some(fruit) <- results) println(fruit)
