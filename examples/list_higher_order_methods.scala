cc4104c8d7207aac1bd4a75f3ff51641a74f95b9
Daniel Miladinov <daniel.miladinov@gmail.com>
2013-08-02 22:57:35 -0400

    Add some examples of flatMap and range, higher order methods of List.

--- examples/list_higher_order_methods.scala
+++ examples/list_higher_order_methods.scala
@@ -13,3 +13,29 @@ words map (_.length)
 words map (_.reverse.mkString)
 // returns: List[String] = List(eht, kciuq, nworb, xof)
 
+// The flatMap operator is similar to map, but it takes a function returning
+// a list of elements as its right operand. It applies the function to each list element
+// and returns the concatenation of all function results.
+// The difference between map and flatMap is illustrated in the following example:
+words map (_.toList)
+// returns: List[List[Char]] = List(List(t, h, e), List(q, u, i, c, k), List(b, r, o, w, n), List(f, o, x))
+
+words flatMap (_.toList)
+// returns: List[Char] = List(t, h, e, q, u, i, c, k, b, r, o, w, n, f, o, x)
+
+// You see that where map returns a list of lists, flatMap returns a single list in which
+// all element lists are concatenated. The differences and interplay between map and flatMap
+// are also demonstrated by the following expression, which constructs a list
+// of all pairs (i, j) such that 1 ≤ j < i < 5:
+List.range(1, 5) flatMap (
+  i => List.range(1, i) map (j => (i, j))
+)
+
+// List.range is a utility method that creates a list of all integers in some range.
+// It is used twice in this example: once to generate a list of integers from 1 (including)
+// until 5 (excluding), and in a second time to generate a list of integers from 1 until i,
+// for each value of i taken from the first list. The map in this expression generates a list
+// of tuples (i, j) where j < i. The outer flatMap in this example generates this list
+// for each i between 1 and 5, and then concatenates all the results. Note that the same list
+// can alternatively be constructed with a for expression:
+for (i <- List.range(1, 5); j <- List.range(1, i)) yield (i, j)
