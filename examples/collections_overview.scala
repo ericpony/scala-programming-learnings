99574f694583ba7c6669d778723ceb9bbd3bc591
Daniel Miladinov <daniel.miladinov@gmail.com>
2013-08-17 15:33:06 -0400

    Add a section on StringOps to the collections overview.

--- examples/collections_overview.scala
+++ examples/collections_overview.scala
@@ -95,3 +95,19 @@ arr.length
 
 arr(0)
 // returns: Int = 12
+
+// One other sequence to be aware of is StringOps, which implements many sequence methods. Because Predef has an
+// implicit conversion from String to StringOps, you can treat any string like a sequence. Here’s an example:
+def hasUpperCase(s: String) = s.exists(_.isUpper)
+// returns: hasUpperCase: (s: String)Boolean
+
+hasUpperCase("Robert Frost")
+// returns: Boolean = true
+
+hasUpperCase("e e cummings")
+// returns: Boolean = false
+
+// In this example, the exists method is invoked on the string named s in the hasUpperCase method body.
+// Because no method named "exists" is declared in class String itself, the Scala compiler will implicitly convert
+// s to StringOps, which has the method. The exists method treats the string as a sequence of characters, and will
+// return true if any of the characters are upper case.
