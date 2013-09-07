f2cc3eb40201f9e9f60d6bea6b1493ebff202b5b
Daniel Miladinov <daniel.miladinov@gmail.com>
2013-09-06 23:04:47 -0400

    Reorganize the Simulation class to a format closer to my taste.
    This may be less idiomatic Scala, or not, but it it's my preference to
    group things together by their access specifier and kind
    (public/private, variable/method).

--- src/net/miladinov/simulation/Simulation.scala
+++ src/net/miladinov/simulation/Simulation.scala
@@ -2,30 +2,16 @@ package net.miladinov.simulation
 
 abstract class Simulation {
   type Action = () => Unit
-
   case class WorkItem(time: Int, action: Action)
 
-  private var curtime = 0
-  def currentTime: Int = curtime
-
+  private var curTime = 0
   private var agenda: List[WorkItem] = List()
-  private def insert(ag: List[WorkItem], item: WorkItem): List[WorkItem] = {
-    if (ag.isEmpty || item.time < ag.head.time) item :: ag
-    else ag.head :: insert(ag.tail, item)
-  }
+
+  def currentTime: Int = curTime
 
   def afterDelay(delay: Int)(block: => Unit) {
     val item = WorkItem(currentTime + delay, () => block)
     agenda = insert(agenda, item)
-  }
-
-  private def next() {
-    (agenda: @unchecked) match {
-      case item :: rest =>
-        agenda = rest
-        curtime = item.time
-        item.action()
-    }
   }
 
   def run() {
@@ -33,5 +19,19 @@ abstract class Simulation {
       println(s"*** simulation started, time = $currentTime ***")
     }
     while (! agenda.isEmpty) next()
+  }
+
+  private def insert(ag: List[WorkItem], item: WorkItem): List[WorkItem] = {
+    if (ag.isEmpty || item.time < ag.head.time) item :: ag
+    else ag.head :: insert(ag.tail, item)
+  }
+
+  private def next() {
+    (agenda: @unchecked) match {
+      case item :: rest =>
+        agenda = rest
+        curTime = item.time
+        item.action()
+    }
   }
 }
