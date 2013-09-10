8c7b2d61a95075d7aeedbf2e46b8975e6fcc2e7f
Daniel Miladinov <daniel.miladinov@gmail.com>
2013-09-10 00:22:05 -0400

    Introduce the next phase of my studies with a trait, FunctionalQueue.
    Next, I'll be studying how Scala does type parameterization. The focus
    of the next chapter of my studies will be creating a functional data
    structure, Queue.
    
    For extra credit, I'm going to be also have it implement this trait,
    FunctionalQueue.

---
+++ src/net/miladinov/typeParameterization/FunctionalQueue.scala
@@ -0,0 +1,27 @@
+package net.miladinov.typeParameterization
+
+// A functional queue is a data structure with three operations:
+trait FunctionalQueue[T] {
+  // Returns the first element of the queue
+  def head: T
+
+  // Returns a queue without its first element
+  def tail: FunctionalQueue[T]
+
+  // Returns a new queue with a given element appended at the end
+  def enqueue(element: T): FunctionalQueue[T]
+}
+
+// Unlike a mutable queue, a functional queue does not change its contents when
+// an element is appended. Instead, a new queue is returned that contains the element.
+// The goal of this chapter will be to create a class, which we’ll name Queue, that works
+// like this:
+
+// val q = Queue(1, 2, 3)
+// returns: q: Queue[Int] = Queue(1, 2, 3)
+
+// val q1 = q enqueue 4
+// returns: q1: Queue[Int] = Queue(1, 2, 3, 4)
+
+// q
+// returns: Queue[Int] = Queue(1, 2, 3)
