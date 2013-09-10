fa2ef2934e68a1076211a64f31fbad0042c82aea
Daniel Miladinov <daniel.miladinov@gmail.com>
2013-09-10 00:42:11 -0400

    Add SlowHeadQueue, which implements FunctionalQueue in terms of a reversed List.
    Now enqueue is constant time, but head and tail are not. They now take
    time proportional to the number of elements stored in the queue.
    
    Looking at SlowHeadQueue and SlowAppendQueue, it does not seem easy
    to come up with an implementation that’s constant time for all three
    operations.
    
    In fact, it looks doubtful that this is even possible!

---
+++ src/net/miladinov/typeParameterization/SlowHeadQueue.scala
@@ -0,0 +1,12 @@
+package net.miladinov.typeParameterization
+
+// The problem with SlowAppendQueue is in the enqueue operation. It takes time proportional to
+// the number of elements stored in the queue. If you want  constant time append, you could also try
+// to reverse the order of the elements in the representation list, so that the last element that’s
+// appended comes first in the list. This would lead to the following implementation:
+
+class SlowHeadQueue[T](elements: List[T]) extends FunctionalQueue[T] {
+  def head: T = elements.last
+  def tail: FunctionalQueue[T] = new SlowHeadQueue[T](elements.init)
+  def enqueue(element: T): FunctionalQueue[T] = new SlowHeadQueue[T](element :: elements)
+}
