b094185bd5844c5edcc845a5a6eed660498b4afd
Daniel Miladinov <daniel.miladinov@gmail.com>
2013-09-10 22:26:26 -0400

    Hide the entire implementation behind a BasicQueueImplementation private class.
    Private constructors and private members are one way to hide the
    initial- ization and representation of a class. Another, more radical
    way is to hide the class itself and only export a trait that reveals the
    public interface of the class.
    
    The code here implements this design. All three methods of the
    FunctionalQueue trait are implemented in a subclass
    BasicQueueImplementation, which is itself a private inner class of
    object BasicQueue. This exposes to clients the same information
    as before, but using a different technique.
    
    Instead of hiding individual constructors and methods, this version
    hides the whole implementation class.

--- src/net/miladinov/typeParameterization/BasicQueue.scala
+++ src/net/miladinov/typeParameterization/BasicQueue.scala
@@ -1,26 +1,26 @@
 package net.miladinov.typeParameterization
 
 object BasicQueue {
-  def apply[T](elements: T*) = new BasicQueue(elements.toList, Nil)
-}
+  def apply[T](elements: T*): FunctionalQueue[T] = new BasicQueueImplementation(elements.toList, Nil)
 
-class BasicQueue[T] private (
-  private val leading: List[T],
-  private val trailing: List[T]
-) extends FunctionalQueue[T] {
+  private class BasicQueueImplementation[T] (
+    private val leading: List[T],
+    private val trailing: List[T]
+  ) extends FunctionalQueue[T] {
 
-  private def mirror =
-    if (leading.isEmpty)
-      new BasicQueue(trailing.reverse, Nil)
-    else
-      this
+    private def mirror =
+      if (leading.isEmpty)
+        new BasicQueueImplementation(trailing.reverse, Nil)
+      else
+        this
 
-  def head: T = mirror.leading.head
+    def head: T = mirror.leading.head
 
-  def tail: FunctionalQueue[T] = {
-    val q = mirror
-    new BasicQueue(q.leading.tail, q.trailing)
+    def tail: FunctionalQueue[T] = {
+      val q = mirror
+      new BasicQueueImplementation(q.leading.tail, q.trailing)
+    }
+
+    def enqueue(element: T): FunctionalQueue[T] = new BasicQueueImplementation(leading, element :: trailing)
   }
-
-  def enqueue(element: T): FunctionalQueue[T] = new BasicQueue(leading, element :: trailing)
 }
