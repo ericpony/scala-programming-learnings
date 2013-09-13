fdfe499c4ad29d4fde28f8364b80fd6ec2700033
Daniel Miladinov <daniel.miladinov@gmail.com>
2013-09-12 21:16:31 -0400

    Improve the implementation of BasicQueue's implementation class.
    This new implementation of BasicQueue performs at most one
    trailing to leading adjustment for any sequence of head operations.
    
    What's different with respect to the previous version is that now
    leading and trailing are reassignable variables, and mirror performs
    the reverse copy from trailing to leading as a side-effect on
    the current queue instead of returning a new queue. This side-effect
    is purely internal to the implementation of the BasicQueue operation;
    since leading and trailing are private variables, the effect is not
    visible to clients of BasicQueue.
    
    This new version of BasicQueue still defines purely functional
    objects, in spite of the fact that they now contain reassignable fields.
    All of the state is hidden away and is purely an implementation detail.

--- src/net/miladinov/typeParameterization/BasicQueue.scala
+++ src/net/miladinov/typeParameterization/BasicQueue.scala
@@ -1,26 +1,31 @@
 package net.miladinov.typeParameterization
 
 object BasicQueue {
-  def apply[T](elements: T*): FunctionalQueue[T] = new BasicQueueImplementation(elements.toList, Nil)
+  def apply[T](elements: T*): FunctionalQueue[T] = new ImprovedBasicQueueImplementation(elements.toList, Nil)
 
-  private class BasicQueueImplementation[+T] (
-    private val leading: List[T],
-    private val trailing: List[T]
+  private class ImprovedBasicQueueImplementation[+T] (
+    private[this] var leading: List[T],
+    private[this] var trailing: List[T]
   ) extends FunctionalQueue[T] {
 
     private def mirror =
-      if (leading.isEmpty)
-        new BasicQueueImplementation(trailing.reverse, Nil)
-      else
-        this
+      if (leading.isEmpty) {
+        while (!trailing.isEmpty) {
+          leading = trailing.head :: leading
+          trailing = trailing.tail
+        }
+      }
 
-    def head: T = mirror.leading.head
-
-    def tail: FunctionalQueue[T] = {
-      val q = mirror
-      new BasicQueueImplementation(q.leading.tail, q.trailing)
+    def head: T = {
+      mirror
+      leading.head
     }
 
-    def enqueue[U >: T](element: U): FunctionalQueue[U] = new BasicQueueImplementation[U](leading, element :: trailing)
+    def tail: FunctionalQueue[T] = {
+      mirror
+      new ImprovedBasicQueueImplementation(leading.tail, trailing)
+    }
+
+    def enqueue[U >: T](element: U): FunctionalQueue[U] =  new ImprovedBasicQueueImplementation[U](leading, element :: trailing)
   }
 }
