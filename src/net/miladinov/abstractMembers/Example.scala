ab41bf8176788ac088b26415b801d41b9a0bb5d3
Daniel Miladinov <daniel.miladinov@gmail.com>
2013-09-14 16:22:19 -0400

    Add an example of abstract members in use.
    Like Java, Scala lets you define abstract methods. But Scala goes
    farther and lets you also define abstract vals, vars, and types as well,
    not just methods!
    
    Scala is even more OOP than Java.

---
+++ src/net/miladinov/abstractMembers/Example.scala
@@ -0,0 +1,25 @@
+package net.miladinov.abstractMembers
+
+// The following trait declares one of each kind of abstract member: an abstract
+// type (T), method (transform), val (initial), and var (current):
+trait Abstract {
+  type T
+  def transform(x: T): T
+  val initial: T
+  var current: T
+}
+
+// A concrete implementation of Abstract needs to fill in definitions for each
+// of its abstract members. Here is an example implementation that provides these definitions:
+class Concrete extends Abstract {
+  type T = String
+  def transform(x: String) = x + x
+  val initial = "hi"
+  var current = initial
+}
+
+// The implementation gives a concrete meaning to the type name T by defining it as an alias
+// of type String. The transform operation concatenates a given string with itself, and the initial
+// and current values are both set to "hi". This example gives you a rough first idea of what kinds
+// of abstract members exist in Scala. The remainder of the chapter will present the details and explain
+// what the new forms of abstract members, as well as type members in general, are good for.
