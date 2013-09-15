c60f46c73a8061994143ddc79f24a679a814b6c9
Daniel Miladinov <daniel.miladinov@gmail.com>
2013-09-15 11:55:06 -0400

    Add an example of how abstract parameter-less defs can be overridden with vals.
    The implementation requirements of defs are less strict than with vals.
    Vals need to always return a stable, immutable value. Defs are not
    guaranteed to do so.
    
    However, it is possible to override abstract parameter-less defs with
    vals, because defs just need to always provide a value, which vals also
    do. Defs make no promise of the stability or immutability of their
    return values.

---
+++ src/net/miladinov/abstractMembers/Fruit.scala
@@ -0,0 +1,21 @@
+package net.miladinov.abstractMembers
+
+abstract class Fruit {
+  val v: String // v for val
+  def m: String // m for method
+}
+
+abstract class Apple extends Fruit {
+  val v: String
+  val m: String // OK to override parameter-less def with val
+}
+
+/* This would not compile:
+
+abstract class BadApple extends Fruit {
+  def v: String // Error: overriding v in class Fruit of type String;
+                //        method v needs to be a stable, immutable value
+  def m: String
+}
+
+*/
