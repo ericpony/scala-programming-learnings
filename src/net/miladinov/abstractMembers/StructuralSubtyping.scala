878ccf87bae6b55afedee044b264e3edb760e65a
Daniel Miladinov <daniel.miladinov@gmail.com>
2013-09-18 23:53:44 -0400

    Use a structural type upper bound to define a proper 'using' function.
    Now, the type checker knows that we can call using on any object of type
    T that is a subtype of a type that defines a function of type 'close(): Unit'.

--- src/net/miladinov/abstractMembers/StructuralSubtyping.scala
+++ src/net/miladinov/abstractMembers/StructuralSubtyping.scala
@@ -27,11 +27,9 @@ abstract class StructuralSubtyping {
     }
   }
 
-  // How would we implement using?
-  // def using(thing: AnyRef)(operation: AnyRef => Any): Any
-  def using[T, S] (thing: T)(operation: T => S) = {
+  def using[T <: { def close(): Unit }, S] (thing: T)(operation: T => S) = {
     val result = operation(thing)
-    thing.close() // Type error!
+    thing.close()
     result
   }
 }
