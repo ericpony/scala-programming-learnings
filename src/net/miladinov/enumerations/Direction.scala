69ff15ef9a7b0ffbaa4329f7ac2be31e2e2ba7ed
Daniel Miladinov <daniel.miladinov@gmail.com>
2013-09-19 23:19:48 -0400

    Associate names with enumeration values using a different overloaded variant of the Value method.

--- src/net/miladinov/enumerations/Direction.scala
+++ src/net/miladinov/enumerations/Direction.scala
@@ -5,6 +5,13 @@ package net.miladinov.enumerations
 // such as the one below, then Direction.Value would be different from Color.Value because
 // the path parts of the two types differ.
 
+// Scala's Enumeration class also offers many other features found in the enumeration designs
+// of other languages. You can associate names with enumeration values by using a different
+// overloaded variant of the Value method:
+
 object Direction extends Enumeration {
-  val North, East, South, West = Value
+  val North = Value("North")
+  val East = Value("East")
+  val South = Value("South")
+  val West = Value("West")
 }
