47e5935985830f64ac764d90b0a865c180e9d1ed
Daniel Miladinov <daniel.miladinov@gmail.com>
2013-09-19 23:21:45 -0400

    Add more discussion about enumerations.

--- src/net/miladinov/enumerations/Direction.scala
+++ src/net/miladinov/enumerations/Direction.scala
@@ -15,3 +15,20 @@ object Direction extends Enumeration {
   val South = Value("South")
   val West = Value("West")
 }
+
+// You can iterate over the values of an enumeration via the set returned by the enumeration's
+// values method:
+
+// for (d <- Direction.values) print(d + " ")
+// prints: North East South West
+
+// Values of an enumeration are numbered from 0, and you can find out the number
+// of an enumeration value by its id method:
+// Direction.East.id
+// returns: Int = 1
+
+// It's also possible to go the other way, from a non-negative integer number to
+// the value that has this number as id in an enumeration:
+
+// Direction(1)
+// returns: Direction.Value = East
