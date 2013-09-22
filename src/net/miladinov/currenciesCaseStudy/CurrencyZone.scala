31aef3718916ec1b238e54a0f2c7d2f60cd5ce67
Daniel Miladinov <daniel.miladinov@gmail.com>
2013-09-22 00:41:33 -0400

    Replace AbstractCurrency with CurrencyZone.
    CurrencyZone defines a factory method make(x: Long): Currency for making
    a currency, and defines the basic common elements of a currency in a
    redefined inner AbstractCurrency class.
    
    CurrencyZone also defines Currency as an abstract type, for individual
    CurrencyZone objects to define.

---
+++ src/net/miladinov/currenciesCaseStudy/CurrencyZone.scala
@@ -0,0 +1,16 @@
+package net.miladinov.currenciesCaseStudy
+
+abstract class CurrencyZone {
+  type Currency <: AbstractCurrency
+  def make(x: Long): Currency
+
+  abstract class AbstractCurrency {
+    val amount: Long
+    def designation: String
+    override def toString = s"$amount $designation"
+    def + (that: Currency): Currency =
+      make(this.amount + that.amount)
+    def * (x: Double): Currency =
+      make((this.amount * x).toLong)
+  }
+}
