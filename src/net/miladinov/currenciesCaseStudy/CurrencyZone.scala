6405974b246944e46ec5ed2d5e775cb5fa15098f
Daniel Miladinov <daniel.miladinov@gmail.com>
2013-09-24 00:01:39 -0400

    Add spacing between definitions in CurrencyZone.

--- src/net/miladinov/currenciesCaseStudy/CurrencyZone.scala
+++ src/net/miladinov/currenciesCaseStudy/CurrencyZone.scala
@@ -3,25 +3,34 @@ package net.miladinov.currenciesCaseStudy
 abstract class CurrencyZone {
   type Currency <: AbstractCurrency
   val CurrencyUnit: Currency
+
   def make(x: Long): Currency
 
   abstract class AbstractCurrency {
     val amount: Long
+
     def designation: String
+
     override def toString: String = {
       ((amount.toDouble / CurrencyUnit.amount.toDouble)
         formatted ("%." + decimals(CurrencyUnit.amount) + "f") + " " + designation)
     }
+
     def + (that: Currency): Currency =
       make(this.amount + that.amount)
+
     def * (x: Double): Currency =
       make((this.amount * x).toLong)
+
     def - (that: Currency): Currency =
       make(this.amount - that.amount)
+
     def / (x: Double): Currency =
       make((this.amount / x).toLong)
+
     def / (that: Currency) =
       this.amount.toDouble / that.amount
+
     private def decimals (n: Long): Int =
       if (n == 1) 0 else 1 + decimals(n / 10)
 
@@ -30,4 +39,5 @@ abstract class CurrencyZone {
         other.amount.toDouble * Converter.exchangeRate
           (other.designation)(this.designation)))
   }
+
 }
