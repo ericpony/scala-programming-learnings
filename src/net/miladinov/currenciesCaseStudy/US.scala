f5cda0b86734801eddaff3a239f61f2cfee98a49
Daniel Miladinov <daniel.miladinov@gmail.com>
2013-09-23 23:58:58 -0400

    Add spacing between US CurrencyZone definitions.

--- src/net/miladinov/currenciesCaseStudy/US.scala
+++ src/net/miladinov/currenciesCaseStudy/US.scala
@@ -1,13 +1,17 @@
 package net.miladinov.currenciesCaseStudy
 
 object US extends CurrencyZone {
+
   abstract class Dollar extends AbstractCurrency {
     def designation = "USD"
   }
+
   type Currency = Dollar
+
   def make(cents: Long) = new Dollar {
     val amount = cents
   }
+
   val Cent = make(1)
   val Dollar = make(1)
   val CurrencyUnit = Dollar
