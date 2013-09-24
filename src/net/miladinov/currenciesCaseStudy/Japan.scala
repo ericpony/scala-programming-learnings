5a4b6d7135a0ddd8c1c5795d63cac8bddc51ea6d
Daniel Miladinov <daniel.miladinov@gmail.com>
2013-09-23 23:59:28 -0400

    Add CurrencyZone objects for Europe and Japan.

---
+++ src/net/miladinov/currenciesCaseStudy/Japan.scala
@@ -0,0 +1,17 @@
+package net.miladinov.currenciesCaseStudy
+
+object Japan extends CurrencyZone {
+
+  abstract class Yen extends AbstractCurrency {
+    def designation = "JPY"
+  }
+
+  type Currency = Yen
+
+  def make(yen: Long) = new Yen {
+    val amount = yen
+  }
+
+  val Yen = make(1)
+  val CurrencyUnit = Yen
+}
