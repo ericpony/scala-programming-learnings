b560f49f92fd25c410251211cf10f42379b62789
Daniel Miladinov <daniel.miladinov@gmail.com>
2013-10-01 06:54:36 -0400

    Override head and tail of case class :: with constructor vals
    This works because every case class parameter is implicitly also a field
    of the class (it's like the parameter declaration was prefixed with
    val).
    
    Recall that Scala allows you to implement an abstract parameterless
    method such as head or tail with a field.
    
    So the code here directly uses the parameters head and tail as
    implementations of the abstract methods head and tail that were
    inherited from class List.

--- src/net/miladinov/list/List.scala
+++ src/net/miladinov/list/List.scala
@@ -16,15 +16,11 @@ case object Nil extends List[Nothing] {
   def tail: List[Nothing] = throw new NoSuchElementException("Tail of empty list")
 }
 
-// Class ::, pronounced "cons" for "construct," represents non-empty lists. It’s named that way in order
+// Class ::, pronounced "cons" for "construct," represents non-empty lists. It's named that way in order
 // to support pattern matching with the infix ::. You have seen before that every infix operation in a pattern
 // is treated as a constructor application of the infix operator to its arguments. So the pattern x :: xs is
 // treated as ::(x, xs) where :: is a case class. Here is the definition of the :: class:
 
-final case class :: [T] (hd: T, tl: List[T]) extends List[T] {
+final case class :: [T] (head: T, tail: List[T]) extends List[T] {
   def isEmpty: Boolean = false
-
-  def head: T = hd
-
-  def tail: List[T] = tl
 }
