8a3f6f8eeabc875e515edccb6cfbc2af61fb01ab
Daniel Miladinov <daniel.miladinov@gmail.com>
2013-12-02 22:54:52 -0500

    Add commentary explaining a potential discrepancy
    The discrepancy is between the stated return type of Email.unapply,
    Option[(String, String)] and the apparent violation of that return type
    with the expression Some(user, domain).
    
    It turns out that Scala will desugar an extra pair of parentheses when
    you pass more than value to a function expecting a single argument
    tuple.

--- src/net/miladinov/extractors/Email.scala
+++ src/net/miladinov/extractors/Email.scala
@@ -7,6 +7,8 @@ object Email {
   // The (mandatory) extraction method
   def unapply (email: String): Option[(String, String)] = {
     email split "@" match {
+      // N.B. you can leave off one pair of parentheses when passing a tuple to a function taking 1 argument
+      // this means that Some(user, domain) is equivalent to Some((user, domain))
       case Array(user, domain) => Some(user, domain)
       case _ => None
     }
