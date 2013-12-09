f1fa306a8272899ec3042ed52f86d83ce46ad1f1
Daniel Miladinov <daniel.miladinov@gmail.com>
2013-12-08 23:40:43 -0500

    Add an example of the varag extractor, Domain, in action

--- src/net/miladinov/extractors/Domain.scala
+++ src/net/miladinov/extractors/Domain.scala
@@ -14,4 +14,13 @@ object Domain {
     case Domain("com", "sun", "java") => println("java.sun.com")
     case Domain("net", _*) => println("a .net domain")
   }
+
+  // You can use the Domain extractor to get more detailed information out of email strings.
+  // For instance, to search for an email address named "tom" in some ".com" domain, you could write
+  // the following function:
+
+  def isTomInDotCom(s: String): Boolean = s match {
+    case Email("tom", Domain("com", _*)) => true
+    case _ => false
+  }
 }
