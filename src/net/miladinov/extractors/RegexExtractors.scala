85a7ca3e3104b0a14aaebcd1a5fc7af4fde53074
Daniel Miladinov <daniel.miladinov@gmail.com>
2013-12-12 22:35:25 -0500

    Use Scala raw strings for more legible regex patterns

--- src/net/miladinov/extractors/RegexExtractors.scala
+++ src/net/miladinov/extractors/RegexExtractors.scala
@@ -5,4 +5,7 @@ import scala.util.matching.Regex
 object RegexExtractors {
   // Both java and scala let you write regex like this
   val Decimal = new Regex("(-)?(\\d+)(\\.\\d*)")
+
+  // Scala raw strings let you write regex patterns with fewer escape slashes
+  val RawStringDecimal = new Regex("""(-)?(\d+)(\.\d*)""")
 }
