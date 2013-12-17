aa56dbe9c9d806b90bb842fa921cfbf138f7c2ce
Daniel Miladinov <daniel.miladinov@gmail.com>
2013-12-16 19:21:33 -0500

    Add an example of using annotations in Scala

---
+++ src/net/miladinov/annotations/QuickAndDirty.scala
@@ -0,0 +1,18 @@
+package net.miladinov.annotations
+
+/**
+ * Annotations are allowed on any kind of declaration or definition, including:
+ *
+ * val's
+ * var's
+ * def's
+ * class's
+ * object's
+ * trait's
+ * type's
+ *
+ * The annotation applies to the entirety of the declaration or definition that follows it.
+ */
+@deprecated class QuickAndDirty {
+
+}
