c97e9b0c18b5b3b22b824aa01eee195ad22faddf
Daniel Miladinov <daniel.miladinov@gmail.com>
2013-12-27 16:08:37 -0500

    Add some commentary on Scala's main XML classes

--- src/net/miladinov/xml/XmlLiterals.scala
+++ src/net/miladinov/xml/XmlLiterals.scala
@@ -7,4 +7,12 @@ object XmlLiterals {
     This is some XML.
     Here is a tag: <atag/>
   </a>
+
+  // Some other important XML classes are:
+  // * Class Node is the abstract superclass of all XML node classes
+  // * Class Text is a node holding just text. For example, the "stuff" part of <a>stuff</a> is of class Text.
+  // * Class NodeSeq holds a sequence of Nodes. Many methods in the XML library process NodeSeqs in places you might
+  //   expect them to process individual Nodes. You can still use such methods with individual nodes, however, since
+  //   Node extends from NodeSeq. This may sound weird, but it works out well for XML. You can think of an individual
+  //   Node as a one-element NodeSeq.
 }
