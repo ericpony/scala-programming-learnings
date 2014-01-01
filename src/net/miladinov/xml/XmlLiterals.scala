2cf3094741b3d8e9e2dac96b660bdff823c7ff02
Daniel Miladinov <daniel.miladinov@gmail.com>
2014-01-01 17:12:23 -0500

    Add more examples of arbitrary Scala expressions escaped within Xml

--- src/net/miladinov/xml/XmlLiterals.scala
+++ src/net/miladinov/xml/XmlLiterals.scala
@@ -19,4 +19,30 @@ object XmlLiterals {
   // You are not restricted to writing out the exact XML you want, character for character. You can evaluate Scala code
   // in the middle of an XML literal by using curly braces as an escape. Here is a simple example:
   val escapingExample = <a> {"hello" + " from XML!"} </a>
+
+  // A braces escape can include arbitrary Scala content, including further XML literals. Thus, as the nesting level
+  // increases, your code can switch back and forth between XML and ordinary Scala code. Here's an example.
+  object CarXml {
+    val yearMade = 1979
+
+    val yearMadeXml =
+      <a> { if (yearMade < 2000) <old> {yearMade} </old>
+            else xml.NodeSeq.Empty }
+      </a>
+
+    // Evaluating yearMadeXml would return:
+    // scala> import net.miladinov.xml.XmlLiterals.CarXml
+
+    // CarXml.yearMadeXml
+    // import net.miladinov.xml.XmlLiterals.CarXml
+
+    // scala> res0: scala.xml.Elem =
+    // <a> <old> 1979 </old>
+    //       </a>
+
+    // If the code inside the curly braces evaluates to either an XML node or a sequence of XML nodes, those nodes are
+    // inserted directly as is. In the above example, if yearMade is less than 2000, it is wrapped in <old> tags and
+    // added to the <a> element. Otherwise, nothing is added. Note in the above example that "nothing" as an XML node
+    // is denoted with xml.NodeSeq.Empty.
+  }
 }
