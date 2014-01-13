df2975d17171aedb030dc1c8a1782577e9780915
Daniel Miladinov <daniel.miladinov@gmail.com>
2014-01-12 23:53:43 -0500

    Expand upon the XML pattern match example

--- src/net/miladinov/xml/patternMatching/SimpleExample.scala
+++ src/net/miladinov/xml/patternMatching/SimpleExample.scala
@@ -29,4 +29,34 @@ object SimpleExample {
   //
   // scala> proc(<c>cherry</c>)
   // res2: String = It's something else
+
+  // Most likely this function is not exactly what you want, because it looks precisely for contents consisting of
+  // a single sub-node within the <a> or <b>. Thus it will fail to match in cases like the following:
+
+  // scala> proc(<a>a <em>red</em> apple</a>)
+  // res3: String = It's something else
+  //
+  // scala> proc(<a />)
+  // res4: String = It's something else
+
+  // If you want the function to match in cases like these, you can match against a sequence of nodes
+  // instead of a single one. The pattern for "any sequence" of XML nodes is written '_*'. Visually, this sequence
+  // looks like the wildcard pattern (_) followed by a regex-style Kleene star (*). Here is the updated function
+  // that matches a sequence of sub-elements instead of a single sub-element:
+
+  def betterProc (node: scala.xml.Node): String =
+    node match {
+      case <a>{contents @ _*}</a> => "It's an a: "+ contents
+      case <b>{contents @ _*}</b> => "It's a b: "+ contents
+      case _ => "It's something else."
+    }
+
+  // Notice that the result of the _* is bound to the contents variable by using the @ pattern described earlier.
+  // Here is the new version in action:
+
+  // scala> betterProc(<a>a <em>red</em> apple</a>)
+  // res0: String = It's an a: ArrayBuffer(a , <em>red</em>,  apple)
+  //
+  // scala> betterProc(<a />)
+  // res1: String = It's an a: WrappedArray()
 }
