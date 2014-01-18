4023c49da3b06ed4e2a641b68424b581fe533473
Daniel Miladinov <daniel.miladinov@gmail.com>
2014-01-17 23:12:56 -0500

    Add a mock database and recipe browser

---
+++ src/net/miladinov/recipe/SimpleDatabase.scala
@@ -0,0 +1,13 @@
+package net.miladinov.recipe
+
+object SimpleDatabase {
+  def allFoods = List(Apple, Orange, Cream, Sugar)
+
+  def foodNamed (name: String): Option[Food] = allFoods.find(_.name == name)
+
+  def allRecipes: List[Recipe] = List(FruitSalad)
+}
+
+object SimpleBrowser {
+  def recipesUsing (food: Food) = SimpleDatabase.allRecipes.filter(recipe => recipe.ingredients.contains(food))
+}
