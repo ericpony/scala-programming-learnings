cfbd8feea28f75c318855d054b8468ab9871681a
Daniel Miladinov <daniel.miladinov@gmail.com>
2014-01-19 22:21:47 -0500

    Add the notion of food categories to the recipe database and browser
    Notice how the implementation of SimpleDatabase.allCategories is
    decoupled from its interface; it's a private member of the
    SimpleDatabase module and can be replaced at any time without disrupting
    the public API.

--- src/net/miladinov/recipe/SimpleDatabase.scala
+++ src/net/miladinov/recipe/SimpleDatabase.scala
@@ -6,8 +6,21 @@ object SimpleDatabase {
   def foodNamed (name: String): Option[Food] = allFoods.find(_.name == name)
 
   def allRecipes: List[Recipe] = List(FruitSalad)
+
+  case class FoodCategory (name: String, foods: List[Food])
+
+  private var categories = List(
+    FoodCategory("fruits", List(Apple, Orange)),
+    FoodCategory("misc", List(Cream, Sugar))
+  )
+
+  def allCategories: List[FoodCategory] = categories
 }
 
 object SimpleBrowser {
   def recipesUsing (food: Food): List[Recipe] = SimpleDatabase.allRecipes.filter(recipe => recipe.ingredients.contains(food))
+
+  def displayCategory (category: SimpleDatabase.FoodCategory): Unit = {
+    println(category)
+  }
 }
