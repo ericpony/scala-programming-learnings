b02aa7348da99032d14a8b68f80dfc161c7edfcb
Daniel Miladinov <daniel.miladinov@gmail.com>
2014-03-21 20:19:27 -0400

    Add a simple program that can parse JSON

---
+++ src/net/miladinov/parserCombinators/ParseJSON.scala
@@ -0,0 +1,55 @@
+package net.miladinov.parserCombinators
+
+// JSON, the JavaScript Object Notation, is a popular data interchange format. Let's write a parser for it.
+
+// Here's a grammar that describes the syntax of JSON:
+// value ::= obj | arr | stringLiteral | floatingPointNumber | "null" | "true" | "false".
+// obj ::= "{" [ members ] "}".
+// arr ::= "[" [ values ] "]".
+// members ::= member {"," member}.
+// member ::= stringLiteral ":" value.
+// values ::= value {"," value}.
+
+// A JSON value is an object, array, string, number, or one of the three reserved words null, true, or false.
+// A JSON object is a (possibly empty) sequence of members separated by commas and enclosed in braces. Each member
+// is a string/value pair where the string and the value are separated by a colon. Finally, a JSON array is a sequence
+// of values separated by commas and enclosed in square brackets. As an example, resources/address_book.json contains
+// an address-book formatted as a JSON object.
+
+// Parsing such data is straightforward when using Scala's parser combinators. The complete parser is shown below.
+// This parser follows the same structure as the arithmetic expression parser. It is again a straightforward mapping
+// of the productions of the JSON grammar. The productions use one shortcut that simplifies the grammar: The repsep
+// combinator parses a (possibly empty) sequence of terms that are separated by a given separator string. For instance,
+// repsep(member, ",") parses a comma-separated sequence of member terms. Otherwise, the productions in the parser
+// correspond exactly to the productions in the grammar, as was the case for the arithmetic expression parsers.
+
+import scala.io.Source
+import scala.util.parsing.combinator._
+
+class JSON extends JavaTokenParsers {
+  def value : Parser[Any] = obj | arr | stringLiteral | floatingPointNumber | "null" | "true" | "false"
+  def obj : Parser[Any] = "{"~repsep(member, ",")~"}"
+  def arr : Parser[Any] = "["~repsep(value, ",")~"]"
+  def member: Parser[Any] = stringLiteral~":"~value
+}
+
+// To try out the JSON parsers, we'll change the framework a bit, so that the parser operates on a resource file
+// instead of on the command line:
+
+object ParseJSON extends JSON {
+  def main(args: Array[String]) {
+    val addressBookContents = Source.fromURL(getClass.getResource("address_book.json")).mkString
+    println(parseAll(value, addressBookContents))
+  }
+}
+
+// The main method in this program first loads the contents of address_book.json into addressBookContents.
+// It then parses the characters in that string according to the value production of the JSON grammar.
+// Note that parseAll and parse exist in overloaded variants: both can take a character sequence or alternatively an
+// input reader as second argument.
+
+// If you run the ParseJSON program, you should get:
+// [14.2] parsed: (({~List((("address book"~:)~(({~List((("name"~:)~"John Smith"),
+// (("address"~:)~(({~List((("street"~:)~"10 Market Street"), (("city"~:)~"San Francisco, CA"),
+// (("zip"~:)~"94111")))~})), (("phone numbers"~:)~(([~List("408 338-4238", "408 111-6892"))~]))))~}))))~})
+
