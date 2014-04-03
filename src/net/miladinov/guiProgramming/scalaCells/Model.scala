0c080b9e6adb243656cc73769ce812de525a5ab5
Daniel Miladinov <daniel.miladinov@gmail.com>
2014-04-02 22:12:28 -0400

    Separate the concept of what is displayed from what is entered in a Spreadsheet cell
    . . . albeit in a very crude way, for now.

---
+++ src/net/miladinov/guiProgramming/scalaCells/Model.scala
@@ -0,0 +1,10 @@
+package net.miladinov.guiProgramming.scalaCells
+
+class Model (val height: Int, val width: Int) {
+  case class Cell(row: Int, column: Int)
+
+  val cells = Array.ofDim[Cell](height, width)
+
+  for (i <- 0 until height; j <- 0 until width)
+    cells(i)(j) = Cell(i, j)
+}
--- src/net/miladinov/guiProgramming/scalaCells/Spreadsheet.scala
+++ src/net/miladinov/guiProgramming/scalaCells/Spreadsheet.scala
@@ -9,6 +9,20 @@ class Spreadsheet (val height: Int, val width: Int) extends ScrollPane {
     autoResizeMode = Table.AutoResizeMode.Off
     showGrid = true
     gridColor = new Color(150, 150, 150)
+
+    val cellModel = new Model(height, width)
+    import cellModel._
+
+    override protected def rendererComponent (isSelected: Boolean, focused: Boolean, row: Int, column: Int): Component =
+      if (hasFocus)
+        new TextField(userData(row, column))
+      else
+        new Label(cells(row)(column).toString) { xAlignment = Alignment.Right }
+
+    def userData (row: Int, column: Int): String = {
+      val v = this(row, column)
+      if (v == null) "" else v.toString
+    }
   }
 
   val rowHeader = new ListView((0 until height) map (_.toString)) {
