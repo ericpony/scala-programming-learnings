68bb3118b4a8596b16ba68c0004a34b65055e95b
Daniel Miladinov <daniel.miladinov@gmail.com>
2014-04-07 22:19:58 -0400

    Add a trait for evaluating spreadsheet formulas
    It's still incomplete, in that it defines no operations of its own. That
    will come later.

--- src/net/miladinov/guiProgramming/scalaCells/Model.scala
+++ src/net/miladinov/guiProgramming/scalaCells/Model.scala
@@ -3,6 +3,7 @@ package net.miladinov.guiProgramming.scalaCells
 class Model (val height: Int, val width: Int) {
   case class Cell(row: Int, column: Int) {
     var formula: Formula = Empty
+    var value: Double = 0.0
     override def toString: String = formula.toString
   }
 
