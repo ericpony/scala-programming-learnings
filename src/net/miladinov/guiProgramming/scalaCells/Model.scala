a7165a79dbcec9209f0542443d70961a826d12c1
Daniel Miladinov <daniel.miladinov@gmail.com>
2014-04-10 20:38:40 -0400

    Implement cell/formula change propagation in the spreadsheet Model/Cell classes

--- src/net/miladinov/guiProgramming/scalaCells/Model.scala
+++ src/net/miladinov/guiProgramming/scalaCells/Model.scala
@@ -1,14 +1,40 @@
 package net.miladinov.guiProgramming.scalaCells
 
+import scala.swing.event.Event
+import swing._
+
 class Model (val height: Int, val width: Int) extends Evaluator with Arithmetic {
-  case class Cell(row: Int, column: Int) {
-    var formula: Formula = Empty
-    def value = evaluate(formula)
+
+  case class Cell(row: Int, column: Int) extends Publisher {
     override def toString: String = formula match {
       case Textual(s) => s
       case _ => value.toString
     }
+
+    private var v: Double = 0.0
+    def value: Double = v
+    def value_= (w: Double): Unit = {
+      if (! (v == w || v.isNaN && w.isNaN)) {
+        v = w
+        publish(new ValueChanged(this))
+      }
+    }
+
+    private var f: Formula = Empty
+    def formula: Formula = f
+    def formula_= (f: Formula): Unit = {
+      for (c <- references(formula)) deafTo(c)
+      this.f = f
+      for (c <- references(formula)) listenTo(c)
+      value = evaluate(f)
+    }
+
+    reactions += {
+      case ValueChanged(_) => value = evaluate(formula)
+    }
   }
+
+  case class ValueChanged(cell: Cell) extends Event
 
   val cells = Array.ofDim[Cell](height, width)
 
